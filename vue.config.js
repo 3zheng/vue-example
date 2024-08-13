6f83148e8fd1fb76518298dcfcede0896a4541d4
3zheng <914509007@qq.com>
2024-08-13 14:24:30 -0400

    vue练习

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    //contentBase:path.join(__dirname, 'public'),
+    port:8888,
+  },
 })
