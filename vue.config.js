2889e2e25f7675874211a87195d073eb19e16560
3zheng <914509007@qq.com>
2024-08-15 09:35:37 -0400

    提交vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  
   devServer:{
     //contentBase:path.join(__dirname, 'public'),
     port:8888,
